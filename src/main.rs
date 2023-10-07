9738e6d1c369ab0f58421540f9f241086b71f048
sacredtreasure <selfharm777@protonmail.com>
2023-10-07 12:28:35 +0100

    err-with-unwrap

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,52 @@
+extern crate serde_json;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
+
 
 #[derive(Serialize, Deserialize)]
 struct Monotype {
     title: String,
-    version: f64,
-    packet_wt: i32,
+    version: String,
+    packet_wt: String,
 }
 #[derive(Serialize, Deserialize)]
 struct Prototype {
     creator: String,
-    version_control: f64,
+    version_control: String,
     class: String,
-    monotype: Vec<Monotype>,
+    monotype: Vec<Monotype>
 }
 
 fn main() {
     let json = r#"
     {
         "creator": "Steven Spielberg",
-        "version control": 1.1,
+        "version control": "1.1",
         "class": "S-class",
         "monotype": [
             {
-                "title" : "Jurassic Park",
-                "version": 1993,
-                "packet_wt": 4,
-            }
+                "title": "Jurassic Park",
+                "version": "1993",
+                "packet_wt":" 4",
+            },
             {
-                "title" : "The Adventures of Tintin",
-                "version": 2011,
-                "packet_wt": 3,
-            }
+                "title": "The Adventures of Tintin",
+                "version": "2011",
+                "packet_wt": "3",
+            },
             {
-                "title" : "The BFG",
-                "version": 2016,
-                "packet_wt": 4,
-            }
+                "title": "The BFG",
+                "version": "2016",
+                "packet_wt": "4",
+            },
             {
-                "title" : "Ready Player One",
-                "version": 2018,
-                "packet_wt": 5,
-            }
+                "title": "Ready Player One",
+                "version": "2018",
+                "packet_wt": "5",
+            },
             {
-                "title" : "The Fabelmans",
-                "version": 2022,
-                "packet_wt": 2,
+                "title": "The Fabelmans",
+                "version": "2022",
+                "packet_wt": "2",
             }
         ]
     }"#;
@@ -58,6 +59,6 @@ fn main() {
 }
 
 fn read_json_typed(raw_json: &str) -> Prototype {
-    let screened: Prototype = serde_json::from_str(raw_json).unwrap();
-    return screened;
+    let screened= serde_json::from_str(raw_json).unwrap();
+    return screened
 }
